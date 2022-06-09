9d3e78e9a4a59d542fdabf5cf73d0610f5f160a1
arhussain1 <a.r.hussain@hotmail.com>
2022-06-09 15:39:04 +0100

    set up react

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ MongoClient.connect(
         console.error(err.stack)
         process.exit(1)
     })
-    .then(async client => {
+    .then(client => {
         app.listen(port, () => {
             console.log(`listening on port ${port}`)
         })
